8d045e4e819bb51ca60aad7c4c37e8ef0d422020
ayush <ayush.srivastava@niit.com>
2026-07-31 14:33:22 +0530

    adding template

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  description = "AWS region where resources will be deployed"
+  type = string
+  default = "ap-south-1"
+}
+variable "project_name" {
+  description = "Project name used for resource naming and tagging"
+  type = string
+  default = "terraform-ec2"
+}
+variable "environment" {
+  description = "Deployment environment"
+  type = string
+  default = "dev"
+}
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+  default = "t3.micro"
+}
+variable "allowed_ssh_cidr" {
+  description = "CIDR allowed to access SSH."
+  type = string
+}
